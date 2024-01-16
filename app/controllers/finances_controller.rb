62404865407d2e284f7f9f9b43f6bd4beae42efa
kenney84 <tkenney930w@gmail.com>
2024-01-15 22:54:28 -0500

    Added a navigation bar template from Tailwind.

--- app/controllers/finances_controller.rb
+++ app/controllers/finances_controller.rb
@@ -1,4 +1,10 @@
 class FinancesController < ApplicationController
   def dashboard
   end
+  def income
+  end
+  def one_time_transactions
+  end
+  def recurring_transactions
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
   get 'finances/dashboard'
-  root 'finances#dashboard'
+  get 'finances/income'
+  get 'finances/one-time-transactions'
+  get 'finances/recurring-transactions'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  root 'finances#dashboard'
   # Defines the root path route ("/")
   # root "posts#index"
 end
