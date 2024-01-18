bcdc3aa92243e8a5195a14497d506de1a2b86104
Tyler J. Kenney <tkenney930@gmail.com>
2024-01-18 16:41:07 -0500

    Added User Authentication Timeout (1 Hour)

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,10 @@ class User < ApplicationRecord
   }.freeze
 
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :timeoutable
 
   validates :password, password_complexity: true
 
